315b3b42e0af08d6eea7bdc22813afbfb0d3c1df
mariusz jurczenko <mariuszjurczenko@gmail.com>
2017-10-21 13:15:36 +0200

    Nasza Pierwsz klasa odcinek.7

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        // Stan (zmienne - pola)
+        List<float> ratings;
+
+
+        // Zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count();
+
+            return avg;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
