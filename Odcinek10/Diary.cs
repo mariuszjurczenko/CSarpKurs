574b52feb89614bbc3a8844f9810be153c3bde02
mariusz jurczenko <mariuszjurczenko@gmail.com>
2017-11-11 08:14:12 +0100

    odc.10

---
+++ Odcinek10/Diary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Odcinek10
+{
+    class Diary
+    {
+        public Diary()
+        {
+            ratings = new List<float>();
+        }
+
+        // Stan (zmienne - pola)
+        List<float> ratings;
+
+
+        // Zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count();
+
+            return avg;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ Odcinek10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            diary.AddRating(3);
+            diary.AddRating(5);
+
+            Diary diary2 = diary;
+            diary2.AddRating(8);
+
+            Diary diary3 = diary2;
+            diary3.AddRating(1);
+
+
+            int liczba = 2;
+        }
+    }
+}
