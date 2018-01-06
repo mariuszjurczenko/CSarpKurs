d2610df1f30ca00c87b39597f121438b61de7d25
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-01-06 11:07:18 +0100

    odcinek 15. Typy referencyjne.

---
+++ Typy/DiaryStatistics.cs
@@ -0,0 +1,9 @@
+﻿namespace Typy
+{
+    public class DiaryStatistics
+    {
+        public float AverageGrade;
+        public float MaxGrade;
+        public float MinGrade;
+    }
+}
---
+++ Typy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Typy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diary diary = new Diary();
+            //diary.AddRating(3f);
+            //diary.AddRating(5f);
+            //diary.AddRating(11);
+
+            //Diary diary2 = new Diary();
+            //Diary diary3 = new Diary();
+            //Diary diary4 = new Diary();
+            //Diary diary5 = new Diary();
+            //Diary diary6 = new Diary();
+
+            //DiaryStatistics stats = diary.ComputeStatistics();
+
+            Diary d1 = new Diary();
+            Diary d2 = d1;
+
+            d1 = new Diary();
+            d1.Name = "Dzienniczek Marcina";
+            Console.WriteLine(d2.Name);
+
+        }
+    }
+}
