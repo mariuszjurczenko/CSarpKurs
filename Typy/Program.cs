5a09ed3e7dab833e0212462aa7c255cddf1c9d59
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-01-20 09:59:32 +0100

    odc.17

--- Typy/Program.cs
+++ Typy/Program.cs
@@ -4,33 +4,30 @@ namespace Typy
 {
     class Program
     {
+        static void GiveName(ref Diary diary)
+        {
+            diary = new Diary();
+            diary.Name = " Nowy Dzienniczek Marcina";
+        }
+
+        static void IncrementNumber(out int number)
+        {
+            number = 42;
+        }
+
         static void Main(string[] args)
         {
-            //Diary diary = new Diary();
-            //diary.AddRating(3f);
-            //diary.AddRating(5f);
-            //diary.AddRating(11);
-
-            //Diary diary2 = new Diary();
-            //Diary diary3 = new Diary();
-            //Diary diary4 = new Diary();
-            //Diary diary5 = new Diary();
-            //Diary diary6 = new Diary();
-
-            //DiaryStatistics stats = diary.ComputeStatistics();
-
             Diary d1 = new Diary();
             Diary d2 = d1;
 
-            d1 = new Diary();
-            d1.Name = "Dzienniczek Marcina";
-            Console.WriteLine(d2.Name);
-            
+            GiveName(ref d2);
 
-            int x1 = 4;
-            int x2 = x1;
-            x1 = 100;
-            Console.WriteLine(x2);
+            Console.WriteLine(d2.Name);
+
+            int x1;
+            IncrementNumber(out x1);
+
+            Console.WriteLine(x1);
 
         }
     }
