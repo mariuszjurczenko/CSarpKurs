9a79d3ee2fafb2b7909f31108a2ac67ab7a09caf
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-03-30 07:10:28 +0200

    odcinek 27

---
+++ FlowControl/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowControl
+{
+    class Program
+    {
+        private static int age;
+        private static string name;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swój wiek: ");
+            age = int.Parse(Console.ReadLine());
+        
+            Console.WriteLine("Podaj swoje imie: ");
+            name = Console.ReadLine();
+
+
+            string test = age > 18 ? "Pełnoletni" : "Dziecko";
+            Console.WriteLine(test);
+
+
+            if (age <= 10)
+            {
+                if (name == "Marcin")
+                {
+                    Method1();
+                }
+            }
+            else if (age < 20)
+            {
+                Method2();
+            }
+            else if (age < 30)
+            {
+                Method3();
+            }
+            else
+            {
+                Method4();
+            }
+
+            Console.ReadKey();
+
+            }
+
+        private static void Method4()
+        {
+            Console.WriteLine("Masz 30 lub wiecej lat");
+        }
+
+        private static void Method3()
+        {
+            Console.WriteLine("Twój wiek jest wiekszy lub równy 20 a mniejszy od 30 i wynosi " + age);
+        }
+
+        private static void Method2()
+        {
+            Console.WriteLine("Twój wiek jest wiekszy od 10 a mniejszy od 20 i wynosi " + age);
+        }
+
+        private static void Method1()
+        {
+            Console.WriteLine("Masz 10 lub mniej lat a dokładnie masz " + age);
+        }
+    }
+}
